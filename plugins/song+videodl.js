e8cc7d5e18c06199774dcfe490d00d22be6d4c01
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 15:54:45 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -30,17 +30,17 @@ async (conn, mek, m, {
         let desc = `
 🧬𝐐𝐔𝐄𝐄𝐍 𝐂𝐇𝐄𝐓𝐇𝐈 𝐘𝐓 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑🧬
 
-`TITLE` 🔍: ${data.title}
+*TITLE* 🔍: ${data.title}
 
-`DESCRIPTION` 🗒️: ${data.description}
+*DESCRIPTION* 🗒️: ${data.description}
 
-`TIME` ⌛: ${data.timestamp}
+*TIME* ⌛: ${data.timestamp}
 
-`AGO` ☄️: ${data.ago}
+*AGO* ☄️: ${data.ago}
 
-`VIEWS` 📽️: ${data.views}
+*VIEWS* 📽️: ${data.views}
 
-ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʜᴀʀᴜᴋᴀ ᴍᴀʜᴇꜱʜ
+*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʜᴀʀᴜᴋᴀ ᴍᴀʜᴇꜱʜ*
         `;
 
         await conn.sendMessage(from, {

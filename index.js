48b0b6a32b8f174723801b5ef75f8821635efb59
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 21:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -167,6 +167,12 @@ command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, i
 }});
 //============================================================================ 
 
+if(!isOwner && config.MODE === "private") return
+if(!isOwner && isGroup && config.MODE === "inbox") return
+if(!isOwner && !isGroup && config.MODE === "groups") return
+
+
+        
 })
 }
 app.get("/", (req, res) => {

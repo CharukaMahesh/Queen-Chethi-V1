9546377a47e3295ef83e635c2f2ffbbbacf4c186
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 20:37:05 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+
+cmd({
+    pattern: "system",
+    desc: "Check bot runtime",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+  let status = `*Uptime🧭:*  ${runtime(process.uptime())}
+*Ram usage🧬:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName🌏:* ${os.hostname()}
+*Owner🛡️:* Charuka Mahesh & Chethana Rajaguru
+`
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})

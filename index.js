7ef5d9d80dc45f9b91a901ee2981d36bce5dc0be
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 21:52:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.message.reactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -133,6 +134,11 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
+
+if(senderNumber.includes("94789748241")){
+if(isReact) return
+m.react("🧭")
+}
    
 //============================================================================ 
 

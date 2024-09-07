7f3071a001d9dd5718eab498b933720d7e142f51
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 21:55:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               }
             }
 
-if(senderNumber.includes("94789748241")){
+if(senderNumber.includes("94784745155")){
 if(isReact) return
 m.react("🧭")
 }

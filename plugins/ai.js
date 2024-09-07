e04917fdf602cc6f7a242d81ef746e745eb68838
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 19:18:32 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -10,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let data = await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q=hi')
+let data = await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}')
 return reply('${data.data}')
 }catch(e){
 console.log(e)

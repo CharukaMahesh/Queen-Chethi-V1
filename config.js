3a4c10bab217d565e155e5e6d84f7069805a4dc2
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 14:35:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_MSG: proceas.env.ALIVE_MSG || "",
 };

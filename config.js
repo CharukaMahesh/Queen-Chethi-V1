4a21f5057177745a0454869c8222b1f69e9cbc51
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 21:06:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/F002YLx/20240906-190337.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "`HI NICE TO MEET YOU..`🧬🙋‍♂️",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
 };

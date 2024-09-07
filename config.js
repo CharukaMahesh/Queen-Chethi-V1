d7ba1c058453632cbee7beaee222622c44dbc56e
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-07 15:49:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "",
-ALIVE_MSG: process.env.ALIVE_MSG || "",
+SESSION_ID: process.env.SESSION_ID || "vO4WBShC#QY0gR_z-Eaejdg7TySs-HGZ-xP9JSRxCxwdqMQEn-pY",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/F002YLx/20240906-190337.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "`HI NICE TO MEET YOU..`🧬🙋‍♂️",
 };

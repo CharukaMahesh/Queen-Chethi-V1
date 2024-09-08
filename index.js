a2415b8d599c428820c9314508177cf8522b5abb
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-08 20:49:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,9 +69,9 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-let up = `👸𝐐𝐔𝐄𝐄𝐍 𝐂𝐇𝐄𝐓𝐇𝐈 𝐌𝐃 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 𝐓𝐎 𝐓𝐇𝐄 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏👸 ✅\n\nPREFIX: ${prefix}`;
+let up = `👸𝐐𝐔𝐄𝐄𝐍 𝐂𝐇𝐄𝐓𝐇𝐈 𝐌𝐃 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 𝐓𝐎 𝐓𝐇𝐄 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏👸`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.ibb.co/F002YLx/20240906-190337.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://raw.githubusercontent.com/CharukaMahesh/Queen-Chethi-V1/main/Img/20240906_190337.jpg` }, caption: up })
 
 }
 })
